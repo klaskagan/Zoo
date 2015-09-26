bf08c7a198dbe1cb0860b10819dec40223b21532
klaskagan <klaskagan@gmail.com>
2015-09-26 16:12:04 +0300

    Finished lesson 04-02.Created image gallery

--- app/src/main/java/com/klaskagan/zoo/fragments/GalleryFragment.java
+++ app/src/main/java/com/klaskagan/zoo/fragments/GalleryFragment.java
@@ -1,9 +1,14 @@
 package com.klaskagan.zoo.fragments;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
 import com.klaskagan.zoo.R;
+import com.klaskagan.zoo.adapters.GalleryImageAdapter;
 import com.klaskagan.zoo.models.GalleryImage;
 import com.klaskagan.zoo.utils.GalleryApiInterface;
 import retrofit.Callback;
@@ -17,7 +22,10 @@ import java.util.List;
  * @author Viktoras Baracevicius
  * @since 2015/09/25.
  */
-public class GalleryFragment extends ListFragment {
+public class GalleryFragment extends Fragment {
+
+    private GridView mGridView;
+    private GalleryImageAdapter mAdapter;
 
     public static GalleryFragment getInstance() {
         GalleryFragment instance = new GalleryFragment();
@@ -28,6 +36,9 @@ public class GalleryFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        mAdapter = new GalleryImageAdapter(getActivity(), 0);
+        mGridView.setAdapter(mAdapter);
+
         RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(getString(R.string.gallery_feed)).build();
         GalleryApiInterface galleryApiInterface = restAdapter.create(GalleryApiInterface.class);
         galleryApiInterface.getStreams(new Callback<List<GalleryImage>>() {
@@ -37,10 +48,8 @@ public class GalleryFragment extends ListFragment {
                     return;
                 }
 
-                for (GalleryImage image : galleryImages) {
-                    Log.i("GalleryFragment", image.getThumbnail());
-                }
-
+                mAdapter.addAll(galleryImages);
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -48,5 +57,16 @@ public class GalleryFragment extends ListFragment {
                 Log.w("GalleryFragment", error.getMessage());
             }
         });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gallery_grid, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mGridView = (GridView) view.findViewById(R.id.grid);
     }
 }
