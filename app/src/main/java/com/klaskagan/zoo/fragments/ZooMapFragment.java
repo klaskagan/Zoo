967511900fd362d96672afa70bee1e4a53b74519
klaskagan <klaskagan@gmail.com>
2015-09-27 23:33:34 +0300

    Finished lesson 05-03. Added markers to the map

--- app/src/main/java/com/klaskagan/zoo/fragments/ZooMapFragment.java
+++ app/src/main/java/com/klaskagan/zoo/fragments/ZooMapFragment.java
@@ -3,7 +3,14 @@ package com.klaskagan.zoo.fragments;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.klaskagan.zoo.R;
 import com.klaskagan.zoo.models.Pin;
 import com.klaskagan.zoo.utils.PinsApiInterface;
@@ -18,7 +25,10 @@ import java.util.List;
  * @author Viktoras Baracevicius
  * @since 2015/09/25.
  */
-public class ZooMapFragment extends SupportMapFragment {
+public class ZooMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+    public static final double LATITUDE = 39.7500;
+    public static final double LONGITUDE = -104.9500;
 
     public static ZooMapFragment getInstance() {
         return new ZooMapFragment();
@@ -27,6 +37,27 @@ public class ZooMapFragment extends SupportMapFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        CameraPosition position = CameraPosition.builder()
+                .target(new LatLng(LATITUDE, LONGITUDE))
+                .zoom(16f)
+                .bearing(0.0f)
+                .tilt(0.0f)
+                .build();
+
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), null);
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        googleMap.setTrafficEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        MarkerOptions options = new MarkerOptions().position(new LatLng(LATITUDE, LONGITUDE));
+        options.title(getString(R.string.zoo));
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        googleMap.addMarker(options);
 
         final RestAdapter adapter = new RestAdapter.Builder().setEndpoint(getString(R.string.pins_feed)).build();
         PinsApiInterface pinsApiInterface = adapter.create(PinsApiInterface.class);
@@ -35,7 +66,10 @@ public class ZooMapFragment extends SupportMapFragment {
             @Override
             public void success(List<Pin> pins, Response response) {
                 for (Pin pin : pins) {
-                    Log.e("Zoo", pin.getName());
+                    MarkerOptions options = new MarkerOptions().position(new LatLng(Double.valueOf(pin.getLatitude()), Double.valueOf(pin.getLongitude())));
+                    options.title(pin.getName());
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
+                    googleMap.addMarker(options);
                 }
             }
 
@@ -44,6 +78,5 @@ public class ZooMapFragment extends SupportMapFragment {
                 Log.e("ZooMapFragment", error.getMessage());
             }
         });
-
     }
 }
